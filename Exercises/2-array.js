358e4ac11c08f39144164a0c04b86c669d8324a1
Alexander <58190800+Alexander-pixel@users.noreply.github.com>
2021-09-28 09:56:11 +0300

    Update 2-array.js

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const data = [];
+  const get = (i) => data[i];
+  get.pop = () => data.pop();
+  get.push = (x) => data.push(x);
+  return get;
+};
 
 module.exports = { array };
